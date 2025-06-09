44da34b3e052d3c1f2001ad23c17ca20fba522c7
LaxmiN801 <ajaykanimate@gmail.com>
2025-06-09 13:13:47 +0530

    cors origin changed

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const __dirname = path.resolve();
 app.use(express.json({limit: "10mb"}));
 app.use(cookieParser());
 app.use(cors({
-	origin: "http://localhost:5173",
+	origin: process.env.CLIENT_URL,
 	credentials: true
 }));
 
